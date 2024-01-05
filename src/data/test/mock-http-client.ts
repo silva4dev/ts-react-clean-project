cb6d506e702abb5c844ce50e77b9b334bbb373d4
silva4dev <web4dev.lucas@outlook.com>
2024-01-05 20:49:04 -0300

    Revert "test: ensure RemoteAuthentication throws InvalidCredentialsError on 401"
    This reverts commit d83cbaff142022d0f143d7f36000a336fbbf6b3f.

--- src/data/test/mock-http-client.ts
+++ src/data/test/mock-http-client.ts
@@ -1,16 +1,12 @@
 import { HttpPostClient, HttpPostParams } from '@/data/protocols/http/http-post-client'
-import { HttpResponse, HttpStatusCode } from '@/data/protocols/http/http-response'
 
 export class HttpPostClientSpy implements HttpPostClient {
   url?: string
   body?: object
-  response: HttpResponse = {
-    statusCode: HttpStatusCode.noContent
-  }
 
-  async post (params: HttpPostParams): Promise<HttpResponse> {
+  async post (params: HttpPostParams): Promise<void> {
     this.url = params.url
     this.body = params.body
-    return await Promise.resolve(this.response)
+    await Promise.resolve()
   }
 }
