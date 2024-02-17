feb1365f89f830f69679812a5b13fbcce075ae71
silva4dev <web4dev.lucas@outlook.com>
2024-02-17 18:11:00 -0300

    chore: config webpack

---
+++ webpack.config.ts
@@ -0,0 +1,37 @@
+import path from 'path'
+import { Configuration } from 'webpack'
+import { CleanWebpackPlugin } from 'clean-webpack-plugin'
+
+const config: Configuration = {
+  mode: 'development',
+  entry: './src/main/index.tsx',
+  output: {
+    path: path.join(__dirname, 'public/js'),
+    publicPath: '/public/js',
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+    alias: {
+      '@': path.join(__dirname, 'src')
+    }
+  },
+  devServer: {
+    devMiddleware: {
+      writeToDisk: true
+    },
+    static: {
+      directory: './public'
+    },
+    historyApiFallback: true
+  },
+  externals: {
+    react: 'React',
+    'react-dom': 'ReactDOM'
+  },
+  plugins: [
+    new CleanWebpackPlugin()
+  ]
+}
+
+export default config
